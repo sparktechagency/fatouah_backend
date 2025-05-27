0a9f18969433efe4cf7b4bba4e83822e6976966b
Moshfiqur Rahman <studentemam@gmail.com>
2025-05-27 17:18:45 +0600

    feat: add admin controller for create, get, update, status update and delete

---
+++ src/app/modules/admin/admin.controller.ts
@@ -0,0 +1,67 @@
+import catchAsync from '../../../shared/catchAsync';
+import sendResponse from '../../../shared/sendResponse';
+import { AdminServices } from './admin.service';
+
+const createAdmin = catchAsync(async (req, res) => {
+  const adminData = req.body;
+  const result = await AdminServices.createAdminToDB(adminData);
+  sendResponse(res, {
+    success: true,
+    statusCode: 200,
+    message: 'Admin created successfully',
+    data: result,
+  });
+});
+
+const getAdmins = catchAsync(async (req, res) => {
+  const result = await AdminServices.getAdminsFromDB();
+  sendResponse(res, {
+    success: true,
+    statusCode: 200,
+    message: 'Admins are retrieved successfully',
+    data: result,
+  });
+});
+
+const updateAdmin = catchAsync(async (req, res) => {
+  const updatedPayload = req.body;
+  const id = req.params.id;
+  const result = await AdminServices.updateAdminToDB(id, updatedPayload);
+  sendResponse(res, {
+    success: true,
+    statusCode: 200,
+    message: 'Admin is updated successfully',
+    data: result,
+  });
+});
+
+const updateAdminStatus = catchAsync(async (req, res) => {
+  const updatedStatus = req.body;
+  const id = req.params.id;
+  const result = await AdminServices.updateAdminStatusToDB(id, updatedStatus);
+  sendResponse(res, {
+    success: true,
+    statusCode: 200,
+    message: 'Admin status updated successfully',
+    data: result,
+  });
+});
+
+const deleteAdmin = catchAsync(async (req, res) => {
+  const id = req.params.id;
+  const result = await AdminServices.deleteAdminFromDB(id);
+  sendResponse(res, {
+    success: true,
+    statusCode: 200,
+    message: 'Admin is deleted successfully',
+    data: result,
+  });
+});
+
+export const AdminControllers = {
+  createAdmin,
+  getAdmins,
+  updateAdmin,
+  updateAdminStatus,
+  deleteAdmin,
+};
