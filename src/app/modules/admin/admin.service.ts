4b3c8afcc4e682c9dd5d17b7250f402a07fbe5de
Moshfiqur Rahman <studentemam@gmail.com>
2025-05-27 21:12:00 +0600

    feat: add update admin status updating service

--- src/app/modules/admin/admin.service.ts
+++ src/app/modules/admin/admin.service.ts
@@ -2,42 +2,53 @@ import { StatusCodes } from 'http-status-codes';
 import ApiError from '../../../errors/ApiError';
 import { User } from '../user/user.model';
 import { IUser } from '../user/user.interface';
-
+import { USER_ROLES } from '../../../enums/user';
 
 const createAdminToDB = async (payload: IUser) => {
-    const createAdmin: any = await User.create(payload);
-    if (!createAdmin) {
-        throw new ApiError(StatusCodes.BAD_REQUEST, 'Failed to create Admin');
-    }
-    if (createAdmin) {
-        await User.findByIdAndUpdate(
-        { _id: createAdmin?._id },
-        { verified: true },
-        { new: true }
-        );
-    }
-    return createAdmin;
+  const createAdmin: any = await User.create(payload);
+  if (!createAdmin) {
+    throw new ApiError(StatusCodes.BAD_REQUEST, 'Failed to create Admin');
+  }
+  if (createAdmin) {
+    await User.findByIdAndUpdate(
+      { _id: createAdmin?._id },
+      { verified: true },
+      { new: true }
+    );
+  }
+  return createAdmin;
 };
 
 const getAdminFromDB = async (): Promise<IUser[]> => {
-    const admins = await User.find({ role: 'ADMIN' })
-        .select('name email profile contact location');
-    return admins;
+  const admins = await User.find({ role: 'ADMIN' }).select(
+    'name email profile contact location'
+  );
+  return admins;
 };
 
+const updateAdminStatusToDB = async (id: string, status: string) => {
+  const result = await User.findByIdAndUpdate(
+    { _id: id },
+    { status },
+    { new: true }
+  );
+  if (!result) {
+    throw new ApiError(StatusCodes.BAD_REQUEST, 'Failed to update status');
+  }
+  return result;
+};
 
 const deleteAdminFromDB = async (id: any): Promise<IUser | undefined> => {
-    const isExistAdmin = await User.findByIdAndDelete(id);
-    if (!isExistAdmin) {
-        throw new ApiError(StatusCodes.BAD_REQUEST, 'Failed to delete Admin');
-    }
-    return;
+  const isExistAdmin = await User.findByIdAndDelete(id);
+  if (!isExistAdmin) {
+    throw new ApiError(StatusCodes.BAD_REQUEST, 'Failed to delete Admin');
+  }
+  return;
 };
-
-
 
 export const AdminServices = {
   createAdminToDB,
   getAdminFromDB,
+  updateAdminStatusToDB,
   deleteAdminFromDB,
 };
---
+++ src/app/modules/contact/contact.service.ts
@@ -0,0 +1,15 @@
+import { StatusCodes } from 'http-status-codes';
+import ApiError from '../../../errors/ApiError';
+import { IContact } from './contact.interface';
+import { Contact } from './contact.model';
+
+const createContactToDB = async (payload: IContact) => {
+  const isExistContact = await Contact.findOne(payload);
+  if(isExistContact){
+    const result=await Contact.findOneAndUpdate()
+  }
+};
+
+export const ContactServices = {
+  createContactToDB,
+};
