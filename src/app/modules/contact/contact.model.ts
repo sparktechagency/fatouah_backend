48969cd58d1fab064aa3cb3b7c2edc91f93f67f1
Moshfiqur Rahman <studentemam@gmail.com>
2025-05-27 20:52:44 +0600

    feat: create contact model with mongoose model

---
+++ src/app/modules/contact/contact.model.ts
@@ -0,0 +1,28 @@
+import { model, Schema } from 'mongoose';
+import { IContact } from './contact.interface';
+
+const contactSchema = new Schema<IContact>(
+  {
+    phone: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+    },
+    location: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+export const Contact = model<IContact>('Contact', contactSchema);
