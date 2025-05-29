e325a6792191486c3435c36f9649ec13ed0e916d
Moshfiqur Rahman <studentemam@gmail.com>
2025-05-29 15:36:32 +0600

    feat: add create parcel order controller

---
+++ src/app/modules/order/order.controller.ts
@@ -0,0 +1,22 @@
+import catchAsync from '../../../shared/catchAsync';
+import sendResponse from '../../../shared/sendResponse';
+import { OrderServices } from './order.service';
+
+export const createParcelOrder = catchAsync(async (req, res) => {
+  const user = req.user;
+  const parcelOrderData = req.body;
+  const result = await OrderServices.createParcelOrderToDB(
+    user,
+    parcelOrderData,
+  );
+  sendResponse(res, {
+    success: true,
+    statusCode: 200,
+    message: 'Parcel order is created successfully',
+    data: result,
+  });
+});
+
+export const OrderControllers = {
+  createParcelOrder,
+};
